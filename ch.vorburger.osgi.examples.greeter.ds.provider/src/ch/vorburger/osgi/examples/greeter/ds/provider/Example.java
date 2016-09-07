529843b7dff255289ca995c96cd0941536aa1368
Michael Vorburger <mike@vorburger.ch>
2016-09-08 00:16:29 +0200

    New Bnd OSGi project > Component Development

---
+++ ch.vorburger.osgi.examples.greeter.ds.provider/src/ch/vorburger/osgi/examples/greeter/ds/provider/Example.java
@@ -0,0 +1,10 @@
+package ch.vorburger.osgi.examples.greeter.ds.provider;
+
+import org.osgi.service.component.annotations.*;
+
+@Component
+public class Example {
+
+	// TODO: class provided by template
+
+}
